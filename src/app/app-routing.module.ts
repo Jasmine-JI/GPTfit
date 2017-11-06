2a1ce3f81f2c38fc3876235b1e7cbc350f22188c
Roy Lee <roy_lee@alatech.com.tw>
2017-11-06 23:20:53 +0800

    [Web]add bootstrap4 and page404 and intial project structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, Routes, RouterModule } from '@angular/router';
+import { Page404Component } from './shared/page404/page404.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '404', component: Page404Component },
+  { path: '**', redirectTo: '404' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { PortalModule } from './containers/portal/portal.module';
+import { Page404Component } from './shared/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page404Component,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PortalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
