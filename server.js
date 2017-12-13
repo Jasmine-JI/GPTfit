8521975ec0c3a61938e24617b37d1df6002a7749
Roy Lee <roy_lee@alatech.com.tw>
2017-12-13 17:18:23 +0800

    [Web]revise port with 8080

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.use(bodyParser.json())
 app.use(function (req, res, next) {
   req.con = connection;
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://152.101.90.130:4200");
+  res.setHeader("Access-Control-Allow-Origin", "http://152.101.90.130:8080");
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
