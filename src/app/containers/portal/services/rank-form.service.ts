a2510fa1b70c81d0568fc51ab968e7312d771798
Roy Lee <roy_lee@alatech.com.tw>
2017-12-21 17:00:53 +0800

    [Web]add domain environment param

--- src/app/containers/portal/services/rank-form.service.ts
+++ src/app/containers/portal/services/rank-form.service.ts
@@ -1,24 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+
+const domain = environment.domain;
 
 @Injectable()
 export class RankFormService {
   constructor(private http: HttpClient) {}
 
   getRank(params) {
-    return this.http.get('http://152.101.90.130:3000/rankform', { params });
+    return this.http.get(domain + 'rankform', { params });
   }
   getMapOptions() {
-    return this.http.get('http://152.101.90.130:3000/rankform/rankInfo/map');
+    return this.http.get(domain + 'rankform/rankInfo/map');
   }
   getMonths() {
-    return this.http.get('http://152.101.90.130:3000/rankform/rankInfo/month');
+    return this.http.get(domain + 'rankform/rankInfo/month');
   }
   getMapInfos(params) {
-    return this.http.get('http://152.101.90.130:3000/rankform/mapInfo', { params });
+    return this.http.get(domain + 'rankform/mapInfo', { params });
   }
   getEmail(params) {
-    return this.http.get('http://152.101.90.130:3000/rankform/rankInfo/email', { params });
+    return this.http.get(domain + 'rankform/rankInfo/email', { params });
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  MAPBOX_API_KEY: 'pk.eyJ1IjoiYnVkZGFsZWUiLCJhIjoiY2o5aDVmbjdzMGo4bDJ3cGd4bmhzYWsydiJ9.lxzLHzTITbvPGDBDnbIKcw'
+  MAPBOX_API_KEY: 'pk.eyJ1IjoiYnVkZGFsZWUiLCJhIjoiY2o5aDVmbjdzMGo4bDJ3cGd4bmhzYWsydiJ9.lxzLHzTITbvPGDBDnbIKcw',
+  domain: 'http://192.168.1.235:3000/'
 };
