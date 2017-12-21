03dd5b803bc5ab1e50e454533998faebf1e42d45
Roy Lee <roy_lee@alatech.com.tw>
2017-12-21 17:10:52 +0800

    [Web]add domain environment param

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  MAPBOX_API_KEY: 'pk.eyJ1IjoiYnVkZGFsZWUiLCJhIjoiY2o5aDVmbjdzMGo4bDJ3cGd4bmhzYWsydiJ9.lxzLHzTITbvPGDBDnbIKcw'
+  MAPBOX_API_KEY: 'pk.eyJ1IjoiYnVkZGFsZWUiLCJhIjoiY2o5aDVmbjdzMGo4bDJ3cGd4bmhzYWsydiJ9.lxzLHzTITbvPGDBDnbIKcw',
+  domain: 'http://alatechapp.alatech.com.tw:3000/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,6 @@
 
 export const environment = {
   production: false,
-  MAPBOX_API_KEY: 'pk.eyJ1IjoiYnVkZGFsZWUiLCJhIjoiY2o5aDVmbjdzMGo4bDJ3cGd4bmhzYWsydiJ9.lxzLHzTITbvPGDBDnbIKcw'
+  MAPBOX_API_KEY: 'pk.eyJ1IjoiYnVkZGFsZWUiLCJhIjoiY2o5aDVmbjdzMGo4bDJ3cGd4bmhzYWsydiJ9.lxzLHzTITbvPGDBDnbIKcw',
+  domain: 'http://alatechapp.alatech.com.tw:3000/'
 };
