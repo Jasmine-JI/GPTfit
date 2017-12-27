0729f14a6ef338cc20bbe89bfadd72f112c399e2
Roy Lee <roy_lee@alatech.com.tw>
2017-12-27 22:08:17 +0800

    [Web]create enroll form page and f2e validate

--- src/app/containers/dashboard/dashboard.module.ts
+++ src/app/containers/dashboard/dashboard.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedComponentsModule } from '@shared/components/shared-components.module';
+import { EnrollFormComponent } from './components/enroll-form/enroll-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    SharedComponentsModule
+    SharedComponentsModule,
+    FormsModule
   ],
   declarations: [
     DashboardComponent,
+    EnrollFormComponent,
   ],
 })
 export class DashboardModule { }
