a0d74416e39107a090bb9e6f87aec4990e35694d
Roy Lee <roy_lee@alatech.com.tw>
2018-02-14 12:00:46 +0800

    [Web]初步完成可上傳到strava的菇能

--- server/models/write_gpx.js
+++ server/models/write_gpx.js
@@ -1,20 +1,70 @@
 module.exports = function writeGpx(results, fs) {
+
   return new Promise((resolve, reject) => {
     let trkptDatas = results.map(_result => {
-      const { longitude, latitude, altitude, utc } = _result;
-      return `<trkpt lat="${latitude}" lon="${longitude}">
-          <ele>${altitude}</ele>
-          <time>${utc}</time>
-        </trkpt>`;
-    });
+      const {
+        longitude,
+        latitude,
+        altitude,
+        utc,
+        file_name,
+        heart_rate,
+        cadence,
+        pace,
+        calorie,
+        incline
+      } = _result;
+    let timeArr = file_name.split('');
+    timeArr =timeArr.slice(0, 8);
+
+    timeArr.splice(4, 0, '-');
+    timeArr.splice(7, 0, '-');
+    timeArr.splice(10, 0, 'T');
+
+    let timeStr = timeArr.join('');
+    let utcValue = utc;
+    if (utcValue.length !== 8) {
+      utcValue = '0' + utcValue;
+    }
+    timeStr = timeStr + utcValue + '.000Z';
+
+    return `<trkpt lat="${latitude}" lon="${longitude}">
+        <ele>${altitude}</ele>
+        <time>${timeStr}</time>
+        <extensions>
+          <ns3:TrackPointExtension>
+            <ns3:hr>${heart_rate}</ns3:hr>
+            <ns3:cad>${cadence}</ns3:cad>
+            <ns3:pace>${pace}</ns3:pace>
+            <ns3:cal>${calorie}</ns3:cal>
+            <ns3:inc>${incline}</ns3:inc>
+          </ns3:TrackPointExtension>
+        </extensions>
+      </trkpt>`;
+  });
+  const { file_name } = results[0];
+  let timeArr = file_name.split('');
+
+  timeArr.splice(4, 0, '-');
+  timeArr.splice(7, 0, '-');
+  timeArr.splice(10, 0, 'T');
+  timeArr.splice(13, 0, ':');
+  timeArr.splice(16, 0, ':');
+  timeArr.splice(19, 0, '.000Z');
+  const timeStr = timeArr.join('');
     trkptDatas = trkptDatas.join('\n');
     const content = `<?xml version="1.0" encoding="UTF-8"?>
-    <gpx creator="Alatech Connect" version="1.0">
+    <gpx creator="Alatech Connect" version="1.0"
+      xmlns="http://www.topografix.com/GPX/1/1"
+      xmlns:ns3="http://www.alatechcloud.alatech.com.tw"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd"
+    >
       <metadata>
         <link href="alatechcloud.alatech.com.tw">
           <text>Alatech Connect</text>
         </link>
-        <time>2017-11-06T11:35:42.000Z</time>
+        <time>${timeStr}</time>
       </metadata>
       <trk>
         <name>跑步</name>
@@ -24,9 +74,9 @@ module.exports = function writeGpx(results, fs) {
         </trkseg>
       </trk>
     </gpx>`;
-    fs.writeFile('./test.gpx', content, function(err) {
+    fs.writeFile('/var/www/html/dist/test.gpx', content, function(err) {
       if (err) reject(err);
-      else  {
+      else {
         resolve(true);
       }
     });
