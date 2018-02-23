417f7aa9297f9be0ededa4c15e659e364a4e7311
Roy Lee <roy_lee@alatech.com.tw>
2018-02-23 17:48:43 +0800

    [Web]complete device log page

--- src/app/containers/dashboard/components/device-log-detail/device-log-detail.component.ts
+++ src/app/containers/dashboard/components/device-log-detail/device-log-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DeviceLogService } from '../../services/device-log.service';
 import { HttpParams } from '@angular/common/http';
@@ -8,13 +8,22 @@ import {
   PageEvent,
   MatSort,
   Sort,
-  MatPaginatorIntl
+  MatPaginatorIntl,
+  MatDatepickerInputEvent
 } from '@angular/material';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/observable/fromEvent';
 import { Router } from '@angular/router';
+import * as moment from 'moment';
+import {
+  BreakpointObserver,
+  BreakpointState,
+  Breakpoints
+} from '@angular/cdk/layout';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-device-log-detail',
@@ -28,18 +37,38 @@ export class DeviceLogDetailComponent implements OnInit {
   currentPage: PageEvent;
   currentSort: Sort;
   userInfo: string;
+  isHandset$: Observable<boolean>;
+  complexForm: FormGroup;
+  startDate: any;
+  endDate: any;
+  isDateSearch = false;
+  getDataTime: string;
 
   @ViewChild('paginator') paginator: MatPaginator;
   @ViewChild('sortTable') sortTable: MatSort;
-  @ViewChild('filter') filter: ElementRef;
+  @ViewChild('f') form: any;
+
   constructor(
     private route: ActivatedRoute,
     private deviceLogservice: DeviceLogService,
     private matPaginatorIntl: MatPaginatorIntl,
-    private router: Router
-  ) {}
+    private router: Router,
+    private breakpointObserver: BreakpointObserver,
+    private fb: FormBuilder,
+    public _location: Location // 調用location.back()，來回到上一頁
+  ) {
+    this.complexForm = this.fb.group({
+      // 定義表格的預設值
+      startDate: [this.startDate, Validators.required],
+      endDate: [this.endDate, Validators.required]
+    });
+  }
 
   ngOnInit() {
+    this.isHandset$ = this.breakpointObserver
+      .observe(Breakpoints.Handset)
+      .map(match => match.matches);
+
     this.userId = this.route.snapshot.paramMap.get('userId');
 
     // 設定顯示筆數資訊文字
@@ -61,19 +90,6 @@ export class DeviceLogDetailComponent implements OnInit {
 
       return `第 ${startIndex + 1} - ${endIndex} 筆、共 ${length} 筆`;
     };
-
-    Observable.fromEvent(this.filter.nativeElement, 'keyup')
-      .debounceTime(300)
-      .distinctUntilChanged()
-      .subscribe(() => {
-        this.logSource.filter = (this.filter
-          .nativeElement as HTMLInputElement).value;
-      });
-
-    this.logSource.filterPredicate = (data: any, filter: string): boolean => {
-      return data.info.indexOf(filter) !== -1;
-    };
-
     this.currentPage = {
       pageIndex: 0,
       pageSize: 10,
@@ -100,14 +116,52 @@ export class DeviceLogDetailComponent implements OnInit {
     const pageNumber = (this.currentPage.pageIndex + 1).toString();
     const pageSize = this.currentPage.pageSize.toString();
     const sort = this.currentSort.direction;
+    if (this.isDateSearch) {
+      params = params.set('startDate', this.complexForm.get('startDate').value);
+      params = params.set('endDate', this.complexForm.get('endDate').value);
+    }
     params = params.set('userId', this.userId);
     params = params.set('pageNumber', pageNumber);
     params = params.set('pageSize', pageSize);
     params = params.set('sort', sort);
     this.deviceLogservice.fetchLists(params).subscribe(res => {
       this.logSource.data = res.datas;
-      this.userInfo = res.datas[0].info;
+      if (res.datas.length > 0) {
+        this.userInfo = res.datas[0].info;
+      }
       this.totalCount = res.meta.pageCount;
+      this.getDataTime = moment().format('YYYY-MM-DD hh:mm:ss');
     });
   }
+
+  logStartDateChange($event: MatDatepickerInputEvent<moment.Moment>) {
+    // 配合save_device_log的time欄位 是YYYY-MM-DD hh:mm:ss.000000
+    const value = moment($event.value).format('YYYY-MM-DD 00:00:00.000000');
+    this.complexForm.patchValue({ startDate: value });
+  }
+
+  logEndDateChange($event: MatDatepickerInputEvent<moment.Moment>) {
+    // 配合save_device_log的time欄位 是YYYY-MM-DD hh:mm:ss.000000
+    const value = moment($event.value).format('YYYY-MM-DD 23:59:59.000000');
+    this.complexForm.patchValue({ endDate: value });
+  }
+  submit({ value, valid }) {
+    if (valid) {
+      this.isDateSearch = true;
+    } else {
+      this.isDateSearch = false;
+    }
+    this.getLists();
+  }
+  refreshLists(event) {
+    this.currentPage = {
+      pageIndex: 0,
+      pageSize: 10,
+      length: null
+    };
+
+    this.form.resetForm();
+    this.getLists();
+  }
+
 }
