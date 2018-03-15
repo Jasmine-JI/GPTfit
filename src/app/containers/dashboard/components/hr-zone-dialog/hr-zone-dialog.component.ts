5e694558101514554b93cb7e3be301887bdcd15b
Roy Lee <roy_lee@alatech.com.tw>
2018-03-15 15:14:04 +0800

    [Web]add hr-zone-dialog

---
+++ src/app/containers/dashboard/components/hr-zone-dialog/hr-zone-dialog.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-hr-zone-dialog',
+  templateUrl: './hr-zone-dialog.component.html',
+  styleUrls: ['./hr-zone-dialog.component.css']
+})
+export class HrZoneDialogComponent implements OnInit {
+  get userId() {
+    return this.data.userId;
+  }
+  get zones() {
+    return this.data.zones;
+  }
+  get methodName() {
+    if (this.data.method === 1) {
+      return '最大心率法%(%MHR)';
+    }
+    return '儲備心率法%(%HRR)';
+  }
+  constructor(@Inject(MAT_DIALOG_DATA) private data: any) {}
+
+  ngOnInit() {}
+}
