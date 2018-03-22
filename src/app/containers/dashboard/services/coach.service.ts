435b2827b92b9f1f233a94b34195504ec6c12f51
Roy Lee <roy_lee@alatech.com.tw>
2018-03-23 01:39:43 +0800

    tmp about coach2

--- src/app/containers/dashboard/services/coach.service.ts
+++ src/app/containers/dashboard/services/coach.service.ts
@@ -23,4 +23,25 @@ export class CoachService {
   fetchRealTimeData(params) {
     return this.http.get<any>(API_SERVER + 'coach/realTimeData', { params });
   }
+  postRaceTest(body) {
+    const headers = new HttpHeaders();
+    headers.append('Accept', 'application/json');
+    headers.append('Accept-Encoding', 'gzip/deflate');
+    headers.append('Content-Type', 'application/json');
+    headers.append('chartset', 'utf-8');
+    headers.append('Authorization', 'required');
+    headers.append('deviceType', '2');
+    headers.append('deviceName', 'htc one');
+    headers.append('deviceOSVersion', 'android');
+    headers.append('deviceID', 'IMEIxxxxxxx');
+    headers.append('appVersionCode', '4.4.14');
+    headers.append('appVersionName', 'v1.0.0');
+    headers.append('language', 'zh');
+    headers.append('regionCode', 'TW');
+    headers.append('appName', 'AlaCloudRun');
+    headers.append('equipmentSN', 'tradmill');
+    return this.http.post<any>('/race_test', body, {
+      headers
+    });
+  }
 }
