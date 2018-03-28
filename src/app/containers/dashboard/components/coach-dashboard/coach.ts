deff8204e9aafb5b3fe4c20f4a21e6ee8e2c29f7
Roy Lee <roy_lee@alatech.com.tw>
2018-03-28 14:47:52 +0800

    [Web]調整UI介面

---
+++ src/app/containers/dashboard/components/coach-dashboard/coach.ts
@@ -0,0 +1,44 @@
+export const mapInfos = [
+  {
+    name: '艾菲爾鐵塔',
+    target: '慢跑緩坡用',
+    distance: 1.45,
+    incline: '0.30%',
+    category: '繞圈'
+  },
+  {
+    name: '慕尼黑奧林匹克公園',
+    target: '測試爆發力',
+    distance: 1.45,
+    incline: '2%',
+    category: '繞圈'
+  },
+  {
+    name: '居庸關長城',
+    target: '測試耐力',
+    distance: 3.98,
+    incline: '11%',
+    category: '繞圈'
+  },
+  {
+    name: '阿爾卑斯山 艾羅路山路',
+    target: '測試綜合反應',
+    distance: 5.07,
+    incline: '8%',
+    category: '線段'
+  },
+  {
+    name: '希臘 雅典運動場',
+    target: '測試速度',
+    distance: 0.5,
+    incline: '0%',
+    category: '繞圈'
+  },
+  {
+    name: '新北市 萬金石暖身賽',
+    target: '測試耐力',
+    distance: 4.2,
+    incline: '2.7%',
+    category: '線段'
+  }
+];
