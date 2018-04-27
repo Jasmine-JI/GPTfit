af6a57c92caaa5c9cdb2b7bd8d9a2429da17a453
Roy Lee <roy_lee@alatech.com.tw>
2018-04-27 17:40:17 +0800

    [Web]add schedule of map

--- server/routes/map.js
+++ server/routes/map.js
@@ -17,28 +17,55 @@ function httpGet(url, callback) {
 
 
 router.get('/', function(req, res, next) {
-  getMapList().then(mapLists => {
-    const { datas, urls } = mapLists;
-    async.map(urls, httpGet, function(err, response) {
-      if (err) return console.log(err);
-      const maps = response.map((_res, idx) => {
-        const {
-          map: {
-            buildMap: { info },
-            raceRoom,
-            basic
+  const { con } = req;
+  const sql = 'truncate table ??';
+  con.query(sql, 'race_map_info', function(err, rows) {
+    if (err) {
+      throw err;
+    }
+    getMapList().then(mapLists => {
+      const sql2 = `insert into ?? (
+          map_index,
+          race_total_distance,
+          race_elevation,
+          race_average_incline,
+          img_url,
+          left_top_coordinate,
+          right_bottom_coordinate,
+          max_lap_limit,
+          map_name
+      ) values ?
+        `;
+      const { datas, urls } = mapLists;
+      async.map(urls, httpGet, function(err, response) {
+        if (err) return console.log(err);
+        const maps = response.map((_res, idx) => {
+          const {
+            map: {
+              buildMap: { info },
+              raceRoom,
+              basic
+            }
+          } = _res;
+          datas[idx].img_url += info[0].FileName1080p;
+          datas[idx].left_top_coordinate = info[0].leftTopCoordinateLat;
+          datas[idx].right_bottom_coordinate = info[0].rightBottomCoordinateLat;
+          datas[idx].max_lap_limit = raceRoom.info[0].raceLap;
+          datas[idx].map_name = basic.info[0].mapName;
+          return info[0].FileName1080p;
+        });
+        const results = datas.map(_data => Object.values(_data));
+        con.query(sql2, ['race_map_info', results], function(err, rows) {
+          if (err) {
+            throw err;
           }
-        } = _res;
-        datas[idx].imgUrl += info[0].FileName1080p;
-        datas[idx].left_top_coordinate = info[0].leftTopCoordinateLat;
-        datas[idx].right_bottom_coordinate = info[0].rightBottomCoordinateLat;
-        datas[idx].max_lap_limit = raceRoom.info[0].raceLap;
-        datas[idx].map_name = basic.info[0].mapName;
-        return info[0].FileName1080p;
+        });
+        console.log('complete update!!');
+        res.json(datas);
       });
-      res.json(datas);
     });
   });
+
 
 });
 
