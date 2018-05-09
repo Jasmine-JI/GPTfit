cc51e83a87111069acdb183363c37c85e6793b84
Roy Lee <roy_lee@alatech.com.tw>
2018-05-09 15:41:46 +0800

    [Web]add NgProgressModule

--- src/app/containers/portal/components/demo-qrcod/demo-qrcod.component.ts
+++ src/app/containers/portal/components/demo-qrcod/demo-qrcod.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { getUrlQueryStrings } from '@shared/utils/';
 import { QrcodeService } from '../../services/qrcode.service';
 import { HttpParams } from '@angular/common/http';
+import { NgProgress } from 'ngx-progressbar';
 
 @Component({
   selector: 'app-demo-qrcod',
@@ -13,16 +14,23 @@ export class DemoQrcodComponent implements OnInit {
   deviceInfo: any;
   isMainAppOpen = false;
   isSecondAppOpen = false;
-  constructor(private qrcodeService: QrcodeService) {}
+  constructor(
+    private qrcodeService: QrcodeService,
+    private progress: NgProgress
+  ) {}
 
   ngOnInit() {
     const queryStrings = getUrlQueryStrings(location.search);
     this.displayQr = queryStrings;
     let params = new HttpParams();
     params = params.set('device_sn', this.displayQr.device_sn);
+    this.progress.start();
     this.qrcodeService
       .getDeviceInfo(params)
-      .subscribe(res => (this.deviceInfo = res));
+      .subscribe(res => {
+        this.deviceInfo = res;
+        this.progress.done();
+      });
   }
   swithMainApp() {
     this.isMainAppOpen = !this.isMainAppOpen;
