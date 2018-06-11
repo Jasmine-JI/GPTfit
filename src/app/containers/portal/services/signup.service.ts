292b53c45d60558e29cd14e00554dafc414c3141
Roy Lee <roy_lee@alatech.com.tw>
2018-06-11 18:06:44 +0800

    [Web]完成註冊頁面的前端認證，剩signup api還沒串

---
+++ src/app/containers/portal/services/signup.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+
+const { API_SERVER } = environment.url;
+
+@Injectable()
+export class SignupService {
+  constructor(private http: HttpClient) {}
+
+  getSMSVerifyCode(body) {
+    return this.http.post('/api/v1/user/getSMSVerifyCode', body);
+  }
+}
