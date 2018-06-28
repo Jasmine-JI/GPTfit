1780f3570dcc66924b59b84b90a1490b311e8059
Roy Lee <roy_lee@alatech.com.tw>
2018-06-28 18:13:22 +0800

    [Web]tmp group memer detail

--- src/app/containers/dashboard/pipes/group-status.pipe.ts
+++ src/app/containers/dashboard/pipes/group-status.pipe.ts
@@ -2,9 +2,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'groupStatus'})
 export class GroupStatusPipe implements PipeTransform {
-  transform(value: string, args: string[]): any {
+  transform(value: number, args: string[]): any {
     console.log('value: ', value);
-    if (value === '1') {
+    if (value === 1) {
       return '自由加入';
     } return '審核制';
   }
