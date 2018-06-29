a112f3e038975c713336840aaa5bdc298d2d930a
Roy Lee <roy_lee@alatech.com.tw>
2018-06-29 10:31:46 +0800

    [Web]complete group info

--- src/app/containers/dashboard/pipes/group-status.pipe.ts
+++ src/app/containers/dashboard/pipes/group-status.pipe.ts
@@ -3,7 +3,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'groupStatus'})
 export class GroupStatusPipe implements PipeTransform {
   transform(value: number, args: string[]): any {
-    console.log('value: ', value);
     if (value === 1) {
       return '自由加入';
     } return '審核制';
