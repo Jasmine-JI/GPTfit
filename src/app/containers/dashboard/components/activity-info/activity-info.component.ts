d1a1e7bd02138797d0fc20f9d661752c3408808c
Roy Lee <roy_lee@alatech.com.tw>
2018-08-21 10:35:53 +0800

    [Web]add about echarts example

--- src/app/containers/dashboard/components/activity-info/activity-info.component.ts
+++ src/app/containers/dashboard/components/activity-info/activity-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EChartOption } from 'echarts';
+import { chartOption, basicAreaOption, rainOption } from './testDatas';
 
 @Component({
   selector: 'app-activity-info',
@@ -9,72 +10,13 @@ import { EChartOption } from 'echarts';
 export class ActivityInfoComponent implements OnInit {
   echartsIntance: any;
 
-  loading = false;
+  chartLoading = false;
+  basicLoading = false;
+  rainLoading = false;
 
-  chartOption = {
-    title: {
-      text: '堆疊區域圖'
-    },
-    tooltip: {
-      trigger: 'axis'
-    },
-    legend: {
-      data: ['Heart rate', 'Elevation', 'Speed']
-    },
-    toolbox: {
-      feature: {
-        saveAsImage: {}
-      }
-    },
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      containLabel: true
-    },
-    xAxis: [
-      {
-        type: 'category',
-        boundaryGap: false,
-        data: ['0km', '1km', '2km', '3km', '4km', '5km', '6km']
-      }
-    ],
-    yAxis: [
-      {
-        type: 'value'
-      }
-    ],
-    series: [
-      {
-        name: 'Heart rate',
-        type: 'line',
-        stack: '总量',
-        areaStyle: { normal: {} },
-        data: [150, 232, 201, 154, 190, 330, 410]
-      },
-      {
-        name: 'Elevation',
-        type: 'line',
-        stack: '总量',
-        areaStyle: { normal: {} },
-        data: [320, 332, 301, 334, 390, 330, 320]
-      },
-      {
-        name: 'Speed',
-        type: 'line',
-        stack: '总量',
-        label: {
-          normal: {
-            show: true,
-            position: 'top'
-          }
-        },
-        areaStyle: { normal: {} },
-        data: [820, 932, 901, 934, 1290, 1330, 1320]
-      }
-    ]
-  };
-
+  chartOption = chartOption;
+  basicAreaOption = basicAreaOption;
+  rainOption = rainOption;
   constructor() {}
 
   ngOnInit() {}
@@ -87,7 +29,7 @@ export class ActivityInfoComponent implements OnInit {
     this.echartsIntance = ec;
   }
 
-  switch() {
-    this.loading = !this.loading;
+  switch(_loading) {
+    this[_loading] = !this[_loading];
   }
 }
