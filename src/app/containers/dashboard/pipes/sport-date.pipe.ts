1bfd25f97f4889d2e323fa82a983ecfa9119f266
Roy Lee <roy_lee@alatech.com.tw>
2018-10-30 17:19:56 +0800

    [Web]運動列表改成規格上的資歷料顯示定義並增加划水的配速計算

--- src/app/containers/dashboard/pipes/sport-date.pipe.ts
+++ src/app/containers/dashboard/pipes/sport-date.pipe.ts
@@ -5,7 +5,8 @@ export class SportDatePipe implements PipeTransform {
   transform(value: string, args: any): any {
     const type = args;
     if (type === 'date') {
-      return value.slice(0, 10);
+      console.log('!!!!', value.slice(0, 10));
+      return value.slice(5, 10).replace(/-/g, '/');
     } else {
       return value.slice(11, 19);
     }
--- src/app/containers/dashboard/pipes/sport-pace.pipe.ts
+++ src/app/containers/dashboard/pipes/sport-pace.pipe.ts
@@ -3,8 +3,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'sportPace'})
 export class SportPacePipe implements PipeTransform {
   transform(value: string, args: any): any {
-    console.log('value: ', value);
-    console.log('args: ', args);
+    if (+value === 0 && (args === '1' || args === '4' || args === '6')) {
+      return `60'00"`;
+    }
     let yVal = 60 / +value * 60;
     if (args === '1') { // 跑步配速
       const costminperkm = Math.floor(yVal / 60);
@@ -14,6 +15,13 @@ export class SportPacePipe implements PipeTransform {
       return `${timeMin}'${timeSecond}"`;
     } else if (args === '4') {　// 游泳配速
       yVal = (60 / +value) * 60 / 10;
+      const costminperkm = Math.floor(yVal / 60);
+      const costsecondperkm = Math.round(yVal - costminperkm * 60);
+      const timeMin = ('0' + costminperkm).slice(-2);
+      const timeSecond = ('0' + costsecondperkm).slice(-2);
+      return `${timeMin}'${timeSecond}"`;
+    } else if (args === '6') { // 游泳配速
+      yVal = (60 / +value) * 60 / 2;
       const costminperkm = Math.floor(yVal / 60);
       const costsecondperkm = Math.round(yVal - costminperkm * 60);
       const timeMin = ('0' + costminperkm).slice(-2);
