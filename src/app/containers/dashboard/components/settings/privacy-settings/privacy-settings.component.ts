bbc02e2c9f1df9c05b61fb6f1b6d682cd733571d
Roy Lee <roy_lee@alatech.com.tw>
2018-11-29 11:31:24 +0800

    [Web]complete privacy-settings ui layout

--- src/app/containers/dashboard/components/settings/privacy-settings/privacy-settings.component.ts
+++ src/app/containers/dashboard/components/settings/privacy-settings/privacy-settings.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-privacy-settings',
   templateUrl: './privacy-settings.component.html',
-  styleUrls: ['./privacy-settings.component.scss']
+  styleUrls: ['./privacy-settings.component.scss', '../settings.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PrivacySettingsComponent implements OnInit {
-
+  isDisplayBox = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  mouseEnter() {
+    this.isDisplayBox = true;
+  }
+  mouseLeave() {
+    this.isDisplayBox = false;
+  }
 }
