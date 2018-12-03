adc2cfd1759f89808201413473e6aff8b613441d
Roy Lee <roy_lee@alatech.com.tw>
2018-12-03 15:47:17 +0800

    [Web]add privacy area map

--- src/app/containers/dashboard/components/settings/privacy-settings/privacy-settings.component.ts
+++ src/app/containers/dashboard/components/settings/privacy-settings/privacy-settings.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild  } from '@angular/core';
+// import {} from '@types/googlemaps';
+declare var google: any;
 
 @Component({
   selector: 'app-privacy-settings',
@@ -8,9 +10,22 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class PrivacySettingsComponent implements OnInit {
   isDisplayBox = false;
-  constructor() { }
+  @ViewChild('gmap') gmapElement: any;
+  // map: google.maps.Map;
+  map: any;
+  mark: any;
+  constructor() {
+  }
 
   ngOnInit() {
+    const mapProp = {
+      center: new google.maps.LatLng(24.123499, 120.660140),
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+    this.mark = new google.maps.Marker({position: mapProp.center, title: '大安'});
+    this.mark.setMap(this.map);
   }
   mouseEnter() {
     this.isDisplayBox = true;
