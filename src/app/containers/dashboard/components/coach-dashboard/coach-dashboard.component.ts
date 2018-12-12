d17401f2d372f85f11b24e55c334ed65230317fd
Roy Lee <roy_lee@alatech.com.tw>
2018-12-12 18:11:50 +0800

    [Web]加上demo局趨勢圖與成員的功能

--- src/app/containers/dashboard/components/coach-dashboard/coach-dashboard.component.ts
+++ src/app/containers/dashboard/components/coach-dashboard/coach-dashboard.component.ts
@@ -24,9 +24,8 @@ import { stockChart } from 'highcharts/highstock';
 
 import { DomSanitizer } from '@angular/platform-browser';
 import { UtilsService } from '@shared/services/utils.service';
-import * as _ from 'lodash';
 import { getUrlQueryStrings } from '@shared/utils/';
-
+import * as _ from 'lodash';
 export class Message {
   constructor(
     public classMemberDataField: any,
@@ -80,7 +79,8 @@ export class Message {
     ])
   ]
 })
-export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy {
+export class CoachDashboardComponent
+  implements OnInit, AfterViewInit, OnDestroy {
   width = 0;
   height = 0;
   fakeDatas: any;
@@ -95,7 +95,7 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
   renderCards = [];
   coachInfo: string;
   hrSortValues: any;
-  hrMeanValue = 0;
+  meanValue = 0;
   hrColors = ['#2e4d9f', '#2eb1e7', '#92c422', '#f5ab14', '#eb5b19', '#c11920'];
   displaySections = [true, true, true];
   dispalyChartOptions = [false, true, false];
@@ -112,18 +112,20 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
   chartHeight = 0;
   s: any;
   time = 0;
-  @ViewChild('hrChartTarget')
-  hrChartTarget: ElementRef;
+  @ViewChild('hrChartTarget') hrChartTarget: ElementRef;
+  @ViewChild('hrChartTarget1') hrChartTarget1: ElementRef;
+  @ViewChild('hrChartTarget2') hrChartTarget2: ElementRef;
   @ViewChild('carousel')
   carouselElement: ElementRef;
   chart: any; // Highcharts.ChartObject
+  chart1: any;
+  chart2: any;
   heartValues = [];
   socketTimer: any;
   isFirstInit = true;
   currentMemberNum = 0;
   totalCoachInfo: string;
   totalLessonInfo: string;
-
   isLoading = false;
   isClassEnd = false;
   token: string;
@@ -139,7 +141,76 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
     coachName: 'Eve Beardall',
     groupVideoUrl: null
   };
+  series1: any;
+  series2: any;
+  series3: any;
+  chartType = 0;
+  cardSortKind = 0;
   classType: string;
+  sortType = 0;
+  isCardDesc = true;
+  demoData: Array<any> = [
+    {
+      liveHr: 100,
+      calorie: 100,
+      cadence: 0,
+      speed: 0,
+      userName: 'Eve Beardall',
+      colorIdx: 0,
+      userIcon: '/assets/demo/coach.png',
+      imgClassName: 'user-photo--landscape'
+    },
+    {
+      liveHr: 100,
+      calorie: 100,
+      cadence: 0,
+      speed: 0,
+      userName: 'Alexia',
+      colorIdx: 0,
+      userIcon: '/assets/demo/1.png',
+      imgClassName: 'user-photo--landscape'
+    },
+    {
+      liveHr: 100,
+      calorie: 100,
+      cadence: 0,
+      speed: 0,
+      userName: 'Florence',
+      colorIdx: 0,
+      userIcon: '/assets/demo/2.png',
+      imgClassName: 'user-photo--landscape'
+    },
+    {
+      liveHr: 100,
+      calorie: 100,
+      cadence: 0,
+      speed: 0,
+      userName: 'Katherine',
+      colorIdx: 0,
+      userIcon: '/assets/demo/3.png',
+      imgClassName: 'user-photo--landscape'
+    },
+    {
+      liveHr: 100,
+      calorie: 100,
+      cadence: 0,
+      speed: 0,
+      userName: 'Martina',
+      colorIdx: 0,
+      userIcon: '/assets/demo/4.png',
+      imgClassName: 'user-photo--landscape'
+    },
+    {
+      liveHr: 100,
+      calorie: 100,
+      cadence: 0,
+      speed: 0,
+      userName: 'Stephanie',
+      colorIdx: 0,
+      userIcon: '/assets/demo/5.png',
+      imgClassName: 'user-photo--landscape'
+    }
+  ];
   classImage =
     'https://www.healthcenterhoornsevaart.nl/wp-content/uploads/2018/02/combat-630x300.jpg';
   private socket$: WebSocketSubject<any>;
@@ -203,8 +274,22 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
       this.sendBoardCast();
     }
   }
-  ngAfterViewInit(): void {
-    if ((this.classId === '99999' && this.isDemoMode)) {
+  handleDemoinit(type) {
+    let series;
+    if (type === 0) {
+      series = _.cloneDeep(this.series1);
+    } else if (type === 1) {
+      series = _.cloneDeep(this.series2);
+    } else {
+      series = _.cloneDeep(this.series3);
+    }
+    if (series && series[0].data.length > 0) {
+      series = series.sort(
+        (a, b) => a.data[a.data.length - 1][0] - b.data[b.data.length - 1][0]
+      );
+    }
+    console.log('series: ', series);
+    if (this.classId === '99999' && this.isDemoMode) {
       this.classInfo.groupIcon = '/assets/demo/demoClass.jpg';
       this.classInfo.coachAvatar = '/assets/demo/coach.png';
       this.classInfo.groupVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
@@ -212,6 +297,41 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
       );
       this.handleCoachInfo(demoCoachInfo);
       this.handleLessonInfo(demoLessonInfo);
+
+      this.currentMemberNum = 6;
+      this.demoTime = moment().format('YYYY/MM/DD Ahh:mm');
+      this.getDemoData(type);
+    }
+    const hrOptions: any = {
+      chart: {
+        height: 250
+      },
+      exporting: {
+        enabled: false
+      },
+      rangeSelector: {
+        inputEnabled: false,
+        enabled: false
+      },
+      xAxis: {
+        type: 'datetime',
+        dateTimeLabelFormats: {
+          millisecond: '%H:%M:%S.%L',
+          second: '%H:%M:%S',
+          minute: '%H:%M:%S',
+          hour: '%H:%M:%S',
+          day: '%H:%M:%S',
+          week: '%H:%M:%S',
+          month: '%H:%M:%S',
+          year: '%H:%M:%S'
+        }
+      },
+      series
+    };
+    this.initHChart(hrOptions);
+  }
+  ngAfterViewInit(): void {
+    if (this.classId === '99999' && this.isDemoMode) {
       const series = [
         { name: 'Eve Beardall', data: [] },
         { name: 'Alexia', data: [] },
@@ -220,38 +340,12 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
         { name: 'Martina', data: [] },
         { name: 'Stephanie', data: [] }
       ];
-      const hrOptions: any = {
-        chart: {
-          height: 280
-        },
-        exporting: {
-          enabled: false
-        },
-        rangeSelector: {
-          inputEnabled: false,
-          enabled: false
-          // selected: 3
-        },
-        xAxis: {
-          type: 'datetime',
-          dateTimeLabelFormats: {
-            millisecond: '%H:%M:%S.%L',
-            second: '%H:%M:%S',
-            minute: '%H:%M:%S',
-            hour: '%H:%M:%S',
-            day: '%H:%M:%S',
-            week: '%H:%M:%S',
-            month: '%H:%M:%S',
-            year: '%H:%M:%S'
-          }
-        },
-        series
-      };
-      this.initHChart(hrOptions);
-      this.currentMemberNum = 6;
-      this.demoTime = moment().format('YYYY/MM/DD Ahh:mm');
-      this.getDemoData();
+
+      this.series1 = _.cloneDeep(series);
+      this.series2 = _.cloneDeep(series);
+      this.series3 = _.cloneDeep(series);
     }
+    this.handleDemoinit(this.chartType);
   }
   handleVideoUrl(url: string) {
     let finalUrl = '';
@@ -294,6 +388,10 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
       const zoneIdx = fields.findIndex(_field => _field === '133');
       let speedIdx = '';
       let cadenceIdx = '';
+      let calorieIdx = '';
+      if (this.classType === '1' || this.classType === '5') {
+        calorieIdx = fields.findIndex(_field => _field === '289');
+      }
       if (this.classType === '2') {
         speedIdx = fields.findIndex(_field => _field === '113');
         cadenceIdx = fields.findIndex(_field => _field === '161');
@@ -309,8 +407,36 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
           const colorIdx = _data[zoneIdx];
           const cadence = +_data[cadenceIdx] || 0;
           const speed = +_data[speedIdx] || 0;
-          this.chart.series[idx].addPoint([moment().unix() * 1000, liveHr]);
-          sum += liveHr;
+          const calorie = +_data[calorieIdx] || 0;
+          const time = moment().unix() * 1000;
+          if (this.chartType === 0) {
+            this.chart.series[idx].addPoint(
+              [time, liveHr],
+              true,
+              false,
+              1000
+            );
+            sum += liveHr;
+          } else if (this.chartType === 1) {
+            this.chart.series[idx].addPoint(
+              [time, calorie],
+              true,
+              false,
+              1000
+            );
+            sum += calorie;
+          } else {
+            this.chart.series[idx].addPoint(
+              [time, colorIdx],
+              true,
+              false,
+              1000
+            );
+            sum += colorIdx;
+          }
+          this.series1[idx].data.push([time, +liveHr]);
+          this.series2[idx].data.push([time, +calorie]);
+          this.series3[idx].data.push([time, +colorIdx]);
           return {
             liveHr,
             cadence,
@@ -333,14 +459,18 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
         }
       }
     }
-    this.heartValues = this.heartValues.sort((a, b) => b.liveHr - a.liveHr);
-    this.hrMeanValue = Math.round(sum / this.currentMemberNum);
+    this.handleSortCard(this.sortType, this.isCardDesc);
+    console.log('this.currentMemberNum: ', this.currentMemberNum);
+    this.meanValue = Math.round(sum / this.currentMemberNum);
   }
   handleSNInfo(snDatas: any) {
     const body = { token: this.token, pairEquipmentSN: snDatas };
     this.coachService.fetchFitPairInfo(body).subscribe(res => {
       const datas = res.info.deviceInfo;
       const series = [];
+      this.series1 = [];
+      this.series2 = [];
+      this.series3 = [];
       let userIcon = '';
       let userName = '';
       let pairEquipmentSN = '';
@@ -356,6 +486,9 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
           pairEquipmentSN = _snData;
         }
         series.push({ name: userName, data: [] });
+        this.series1.push({ name: userName, data: [] });
+        this.series2.push({ name: userName, data: [] });
+        this.series3.push({ name: userName, data: [] });
         userIcon =
           datas[existIdx] && datas[existIdx].pairIcon.length > 0
             ? this.utils.buildBase64ImgString(datas[existIdx].pairIcon)
@@ -376,7 +509,7 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
       });
       const hrOptions: any = {
         chart: {
-          height: 280
+          height: 250
         },
         exporting: {
           enabled: false
@@ -384,7 +517,6 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
         rangeSelector: {
           inputEnabled: false,
           enabled: false
-          // selected: 3
         },
         xAxis: {
           type: 'datetime',
@@ -408,66 +540,11 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
       );
     });
   }
-  getDemoData() {
-    const demoData: Array<any> = [
-      {
-        liveHr: 100,
-        cadence: 0,
-        speed: 0,
-        userName: 'Eve Beardall',
-        colorIdx: 0,
-        userIcon: '/assets/demo/coach.png',
-        imgClassName: 'user-photo--landscape'
-      },
-      {
-        liveHr: 100,
-        cadence: 0,
-        speed: 0,
-        userName: 'Alexia',
-        colorIdx: 0,
-        userIcon: '/assets/demo/1.png',
-        imgClassName: 'user-photo--landscape'
-      },
-      {
-        liveHr: 100,
-        cadence: 0,
-        speed: 0,
-        userName: 'Florence',
-        colorIdx: 0,
-        userIcon: '/assets/demo/2.png',
-        imgClassName: 'user-photo--landscape'
-      },
-      {
-        liveHr: 100,
-        cadence: 0,
-        speed: 0,
-        userName: 'Katherine',
-        colorIdx: 0,
-        userIcon: '/assets/demo/3.png',
-        imgClassName: 'user-photo--landscape'
-      },
-      {
-        liveHr: 100,
-        cadence: 0,
-        speed: 0,
-        userName: 'Martina',
-        colorIdx: 0,
-        userIcon: '/assets/demo/4.png',
-        imgClassName: 'user-photo--landscape'
-      },
-      {
-        liveHr: 100,
-        cadence: 0,
-        speed: 0,
-        userName: 'Stephanie',
-        colorIdx: 0,
-        userIcon: '/assets/demo/5.png',
-        imgClassName: 'user-photo--landscape'
-      }
-    ];
+  getDemoData(type) {
     let coachLiveHr = 100;
+
     this.demoMaker = setInterval(() => {
-      this.heartValues = demoData;
+      this.heartValues = this.demoData; // 利用demoData 紀錄照原本順序的最新狀態資料
       let sum = 0;
       this.heartValues = this.heartValues.map((_demoData, idx) => {
         if (idx === 0) {
@@ -482,43 +559,111 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
               _demoData.liveHr + (Math.floor(Math.random() * 5) - 1);
           }
           coachLiveHr = _demoData.liveHr;
+
+          _demoData.calorie =
+            _demoData.calorie + (Math.floor(Math.random() * 3) + 1);
         }
         if (idx === 1) {
           _demoData.liveHr =
             coachLiveHr +
             Math.round(1 * (Math.floor(Math.random() * 8.98) - 3.99));
+          _demoData.calorie =
+            _demoData.calorie + (Math.floor(Math.random() * 3) + 1);
         }
         if (idx === 2) {
           _demoData.liveHr =
             coachLiveHr +
             Math.round(2 * (Math.floor(Math.random() * 7.98) - 2.99));
+          _demoData.calorie =
+            _demoData.calorie + (Math.floor(Math.random() * 3) + 1);
         }
         if (idx === 3) {
           _demoData.liveHr =
             coachLiveHr +
             Math.round(5 * (Math.floor(Math.random() * 6.98) - 1.99));
+          _demoData.calorie =
+            _demoData.calorie + (Math.floor(Math.random() * 3) + 1);
         }
         if (idx === 4) {
           _demoData.liveHr =
             coachLiveHr +
             Math.round(6 * (Math.floor(Math.random() * 6.98) - 2.99));
+          _demoData.calorie =
+            _demoData.calorie + (Math.floor(Math.random() * 3) + 1);
         }
         if (idx === 5) {
           _demoData.liveHr =
             coachLiveHr +
             Math.round(3 * (Math.floor(Math.random() * 9.98) - 5.99));
+          _demoData.calorie =
+            _demoData.calorie + (Math.floor(Math.random() * 3) + 1);
         }
         _demoData.colorIdx = this.handleDemoColor(_demoData.liveHr);
-        sum += _demoData.liveHr;
-        this.chart.series[idx].addPoint([
-          moment().unix() * 1000,
-          _demoData.liveHr
-        ]);
+
+        const time = moment().unix() * 1000;
+        if (type === 0) {
+          this.chart.series[idx].addPoint(
+            [time, _demoData.liveHr],
+            true,
+            false,
+            1000
+          );
+          sum += _demoData.liveHr;
+        } else if (type === 1) {
+          this.chart.series[idx].addPoint(
+            [time, _demoData.calorie],
+            true,
+            false,
+            1000
+          );
+          sum += _demoData.calorie;
+        } else {
+          this.chart.series[idx].addPoint(
+            [time, _demoData.colorIdx],
+            true,
+            false,
+            1000
+          );
+          sum += _demoData.colorIdx;
+        }
+        this.series1[idx].data.push([time, _demoData.liveHr]);
+        this.series2[idx].data.push([time, _demoData.calorie]);
+        this.series3[idx].data.push([time, _demoData.colorIdx]);
+
         return _demoData;
       });
-      this.heartValues = this.heartValues.sort((a, b) => b.liveHr - a.liveHr);
-      this.hrMeanValue = Math.round(sum / this.currentMemberNum);
+      this.handleSortCard(this.sortType, this.isCardDesc);
+      this.meanValue = Math.round(sum / this.currentMemberNum);
     }, 5000);
+  }
+  handleSortCard(sortType, status) {
+    this.isCardDesc = status;
+    this.sortType = sortType;
+    if (!this.isCardDesc) {
+      if (this.sortType === 0) {
+        this.heartValues = this.heartValues.sort((a, b) => a.liveHr - b.liveHr);
+      } else if (this.sortType === 1) {
+        this.heartValues = this.heartValues.sort(
+          (a, b) => a.calorie - b.calorie
+        );
+      } else {
+        this.heartValues = this.heartValues.sort(
+          (a, b) => a.colorIdx - b.colorIdx
+        );
+      }
+    } else {
+      if (this.sortType === 0) {
+        this.heartValues = this.heartValues.sort((a, b) => b.liveHr - a.liveHr);
+      } else if (this.sortType === 1) {
+        this.heartValues = this.heartValues.sort(
+          (a, b) => b.calorie - a.calorie
+        );
+      } else {
+        this.heartValues = this.heartValues.sort(
+          (a, b) => b.colorIdx - a.colorIdx
+        );
+      }
+    }
   }
   sendBoardCast() {
     this.isLoading = true;
@@ -531,7 +676,16 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
     clearInterval(this.socketTimer);
     clearInterval(this.demoMaker);
   }
-
+  removeSeries(type) {
+    this.chartType = type;
+    const seriesLength = this.chart.series.length;
+    clearInterval(this.demoMaker);
+    this.meanValue = 0;
+    for (let i = seriesLength - 1; i > -1; i--) {
+      this.chart.series[i].remove();
+    }
+    this.handleDemoinit(this.chartType);
+  }
   initHChart(option) {
     this.chart = stockChart(this.hrChartTarget.nativeElement, option);
   }
@@ -550,9 +704,7 @@ export class CoachDashboardComponent implements OnInit, AfterViewInit, OnDestroy
   handleLessonInfo(str) {
     this.totalLessonInfo = str.replace(/\r\n|\n/g, '').trim();
     if (this.totalLessonInfo.length > 118) {
-      console.log('before : ', this.totalLessonInfo);
       this.lessonInfo = this.totalLessonInfo.substring(0, 118);
-      console.log('after : ', this.totalLessonInfo);
       this.isLessonMoreDisplay = true;
     } else {
       this.lessonInfo = this.totalLessonInfo;
