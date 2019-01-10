25cf3c1446f4de15803168b54e5fb699bc57e957
Roy Lee <roy_lee@alatech.com.tw>
2019-01-10 11:04:14 +0800

    [Web]chabe version v1.0.0.2

--- src/app/shared/version.ts
+++ src/app/shared/version.ts
@@ -1,6 +1,6 @@
 // fix time 2018/01//04
 export const version = {
-  develop: '1.0.0.0 alpha',
-  release: '1.0.0.0 alpha',
+  develop: '1.0.0.2 alpha',
+  release: '1.0.0.2 alpha',
   master: '1.0.0.0 beta'
 };
