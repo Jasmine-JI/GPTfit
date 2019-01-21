bbc9a617c516b6d740c1051981fc78d9d0d8bed9
Roy Lee <roy_lee@alatech.com.tw>
2019-01-21 16:40:50 +0800

    [Web]更改版號

--- src/app/shared/version.ts
+++ src/app/shared/version.ts
@@ -1,6 +1,6 @@
-// fix time 2018/01//04
+// fix time 2018/01/21
 export const version = {
-  develop: '1.0.0.2 alpha',
-  release: '1.0.0.2 alpha',
-  master: '1.0.0.0 beta'
+  develop: '1.0.0.3 alpha',
+  release: '1.0.0.3 alpha',
+  master: '1.0.0.3 beta'
 };
