b8e1db83f3c017bb53ed6268350025c9ce258c6b
Roy Lee <roy_lee@alatech.com.tw>
2019-01-21 17:38:22 +0800

    version code加入dev code

--- src/app/shared/version.ts
+++ src/app/shared/version.ts
@@ -1,6 +1,6 @@
 // fix time 2018/01/21
 export const version = {
-  develop: '1.0.3 alpha',
-  release: '1.0.3 alpha',
-  master: '1.0.3 beta'
+  develop: '1.0.3.1 alpha',
+  release: '1.0.3.1 alpha',
+  master: '1.0.3.1 beta'
 };
