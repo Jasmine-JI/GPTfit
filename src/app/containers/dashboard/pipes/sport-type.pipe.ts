9b1acb287bfc58ed01368381c21581c7f09925dc
Roy Lee <roy_lee@alatech.com.tw>
2019-01-30 17:38:26 +0800

    [Web]調整train live列表排版與樣式

--- src/app/containers/dashboard/pipes/sport-type.pipe.ts
+++ src/app/containers/dashboard/pipes/sport-type.pipe.ts
@@ -3,7 +3,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'sportType'})
 export class SportTypePipe implements PipeTransform {
   transform(value: string, args: string[]): any {
-    const sportTypes = ['跑步', '自行車', '重量訓練', '游泳', '有氧', '划水(雙手,划船)', '尚無相關類別'];
+    const sportTypes = [
+      'Dashboard.SportReport.Run',
+      'Dashboard.SportReport.Cycle',
+      'Dashboard.SportReport.WeightTraining',
+      'Dashboard.SportReport.Swim',
+      'Dashboard.SportReport.Aerobic',
+      'Dashboard.SportReport.Rowing',
+      'SH.No-information'
+    ];
     return sportTypes[+value - 1];
   }
 }
