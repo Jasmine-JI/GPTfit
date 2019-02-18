5126cc3d413eec0ec8af9fdaeb08396b879ce4ea
Roy Lee <roy_lee@alatech.com.tw>
2019-02-18 15:49:43 +0800

    [Web]cloud run leaderboard多語系

--- src/app/shared/pipes/map-translate.pipe.ts
+++ src/app/shared/pipes/map-translate.pipe.ts
@@ -1,24 +1,43 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
-@Pipe({name: 'mapTranslate'})
+@Pipe({ name: 'mapTranslate' })
 export class MapTranslatePipe implements PipeTransform {
-  transform(value: string, args: string[]): any {
-    if (value === '艾菲爾鐵塔路跑') {
-      return 'Tour_Eiffel';
-    } else if (value === '居庸關長城') {
-      return 'Ju-Yong_customs';
-    } else if (value === '歐伯維森菲爾德 奧林匹克體育場') {
-      return 'Olympiapark_Munchen';
-    } else if (value === '帕那辛奈克體育場') {
-      return 'Panathenean_Stadium';
-    } else if (value === '阿爾卑斯山道賽') {
-      return 'Airolo';
-    } else if (value === '繞圈') {
-      return 'circle';
-    } else if (value === '線段') {
-      return 'line';
-    } else if (value === '萬金石暖身賽') {
-      return 'Wanjinshi warm-up';
+  constructor(private translate: TranslateService) {}
+  transform(value: number, args: string[]): any {
+    switch (value) {
+      case 1:
+        return 'SH.Map.EffelTowerRoadRunning';
+      case 2:
+        return 'SH.Map.OberwiesenfeldOlympiapark';
+      case 3:
+        return 'SH.Map.GreatWallRoadRunning';
+      case 4:
+        return 'SH.Map.AlpsHillRoad';
+      case 5:
+        return 'SH.Map.KallimarmaroStadium';
+      case 6:
+        return 'SH.Map.WanjinshiWarmUp';
+      case 7:
+        return 'SH.Map.TheBundInternationalMarathon';
+      case 8:
+        return 'SH.Map.WanjinshiMiniMarathon';
+      case 9:
+        return 'SH.Map.AlishanRoadRunningExperience';
+      case 10:
+        return 'SH.Map.LanyuRoundabout';
+      case 11:
+        return 'SH.Map.LanyuRoundabout';
+      case 12:
+        return 'SH.Map.LanyuRoundabout';
+      case 13:
+        return 'SH.Map.LanyuRoundabout';
+      case 14:
+        return 'SH.Map.LanyuRoundabout';
+      case 15:
+        return 'SH.Map.BeijingMarathon';
+      default:
+        return 'SH.No-information';
     }
   }
 }
