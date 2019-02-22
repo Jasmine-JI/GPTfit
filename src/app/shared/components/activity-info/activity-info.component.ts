a8fe523473aebd2eb1c3ff23ae21174e13b846d4
Roy Lee <roy_lee@alatech.com.tw>
2019-02-22 10:16:25 +0800

    [Web]運動檔案開啟debug mode

--- src/app/shared/components/activity-info/activity-info.component.ts
+++ src/app/shared/components/activity-info/activity-info.component.ts
@@ -622,7 +622,8 @@ export class ActivityInfoComponent implements OnInit, AfterViewInit, OnDestroy {
       this.activityPoints,
       this.activityInfo.type,
       this.resolutionSeconds,
-      hrFormatData
+      hrFormatData,
+      this.isDebug
     );
     this.finalDatas = finalDatas;
 
