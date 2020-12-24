b45e3e1976c1f5700877094105d0651a3017dde8
kidin_liu <dezhi0205@hotmail.com>
2020-12-24 17:30:14 +0800

    [web 1.4.0.5]bug修正和開放g值圖表

---
+++ src/app/shared/components/privacy-setting-dialog/privacy-setting-dialog.component.ts
@@ -0,0 +1,157 @@
+import { Component, OnInit, Inject, EventEmitter, Output, Input } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { UtilsService } from '../../services/utils.service';
+import { SettingsService } from '../../../containers/dashboard/services/settings.service';
+import { TranslateService } from '@ngx-translate/core';
+import { UserProfileService } from '../../services/user-profile.service';
+
+@Component({
+  selector: 'app-privacy-setting-dialog',
+  templateUrl: './privacy-setting-dialog.component.html',
+  styleUrls: [
+    './privacy-setting-dialog.component.scss',
+    '../../../containers/dashboard/components/settings/privacy-settings/privacy-settings.component.scss'
+  ]
+})
+export class PrivacySettingDialogComponent implements OnInit {
+  i18n = {  // 可能再增加新的翻譯
+    gym: ''
+  };
+  groupId: string;
+  token: string;
+  shareTargetText: string;
+  tempActivityTrackingReport: string[];
+  tempActivityTracking: string[];
+  tempActivityTrackingReportStatus: string[];
+  tempActivityTrackingStatus: string[];
+  tempLifeTracking: string[];
+  tempLifeTrackingStatus: string[];
+  showBatchChangeBox = false;
+  @Output() onConfirm: EventEmitter<any> = new EventEmitter();
+
+  get activityTrackingReport() {
+    this.tempActivityTrackingReport = [...this.data.activityTrackingReport];
+    return this.data.activityTrackingReport;
+  }
+  get activityTracking() {
+    this.tempActivityTracking = [...this.data.activityTracking];
+    return this.data.activityTracking;
+  }
+  get activityTrackingReportStatus() {
+    this.tempActivityTrackingReportStatus = [...this.data.activityTrackingReportStatus];
+    return this.data.activityTrackingReportStatus;
+  }
+  get activityTrackingStatus() {
+    this.tempActivityTrackingStatus = [...this.data.activityTrackingStatus];
+    return this.data.activityTrackingStatus;
+  }
+  get targetText() {
+    return this.data.targetText;
+  }
+  get groupName() {
+    return this.data.groupName;
+  }
+  constructor(
+    private dialog: MatDialog,
+    private utils: UtilsService,
+    private settingsService: SettingsService,
+    private userProfileService: UserProfileService,
+    private translate: TranslateService,
+    @Inject(MAT_DIALOG_DATA) private data: any
+  ) {}
+
+  ngOnInit() {
+    this.translate.get('hollow world').subscribe(() => {
+      this.i18n.gym = this.translate.instant('universal_group_gym');
+    });
+
+    const activityTrackingArray = [],
+          activityTrackingReportArray = [],
+          lifeTrackingReportArray = [];
+
+    // 隱私權預設勾選「新運動檔案」及「新運動期間報告」開放給體適能教練
+    activityTrackingArray.push('1');
+    activityTrackingArray.push('4');
+    activityTrackingReportArray.push('1');
+    activityTrackingReportArray.push('4');
+    lifeTrackingReportArray.push('1');
+    lifeTrackingReportArray.push('4');
+
+    this.tempActivityTracking = activityTrackingArray;
+    this.tempActivityTrackingReport = activityTrackingReportArray;
+    this.tempLifeTracking = lifeTrackingReportArray;
+  }
+
+  confirm() {
+    this.onConfirm.emit();
+    let body;
+    if (this.tempActivityTracking.length !== 0) {
+      body = {
+        token: this.utils.getToken() || '',
+        privacy: {
+          activityTracking: this.tempActivityTracking,
+          activityTrackingReport: this.tempActivityTrackingReport,
+          lifeTrackingReport: this.tempLifeTracking
+        }
+      };
+    } else {
+      body = {
+        token: this.utils.getToken() || '',
+        privacy: {
+          activityTracking: this.tempActivityTracking,
+          activityTrackingReport: this.tempActivityTrackingReport
+        }
+      };
+    }
+
+
+    this.settingsService.updateUserProfile(body).subscribe(() => {
+      // 重新存取身體資訊供各種圖表使用-kidin-1081212
+      const refreshBody = {
+        token : this.utils.getToken() || ''
+      };
+      this.userProfileService.refreshUserProfile({
+        token: refreshBody
+      });
+      this.dialog.closeAll();
+    });
+
+  }
+  handleCheckBox(event, idx, type) {
+    let tempArr = [];
+    let tempStatus = [];
+    if (type === 1) {
+      tempArr = [...this.activityTracking];
+      tempStatus = [...this.activityTrackingStatus];
+    } else if (type === 2) {
+      tempArr = [...this.activityTrackingReport];
+      tempStatus = [...this.activityTrackingReportStatus];
+    } else {
+      tempArr = this.tempLifeTracking;
+      tempStatus = this.tempLifeTrackingStatus;
+    }
+    if (event.checked) {
+      tempStatus[idx] = true;
+      tempArr.push(event.source.value);
+    } else {
+      tempStatus[idx] = false;
+      const i = tempArr.findIndex(x => x.value === event.source.value);
+      tempArr.splice(i, 1);
+    }
+    if (type === 1) {
+      this.tempActivityTracking = [...tempArr];
+      this.tempActivityTrackingStatus = [...tempStatus];
+    } else if (type === 2) {
+      this.tempActivityTrackingReport = [...tempArr];
+      this.tempActivityTrackingReportStatus = [...tempStatus];
+    } else {
+      tempArr = this.tempLifeTracking;
+      tempStatus = this.tempLifeTrackingStatus;
+    }
+  }
+
+  // 取得輸入框顯示與否的狀態-kidin-1090326
+  showBox (e) {
+    this.showBatchChangeBox = e;
+  }
+}
