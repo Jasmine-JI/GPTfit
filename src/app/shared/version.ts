42fa133c62abe53190e76f714d4482953d51f455
kidin_liu <dezhi0205@hotmail.com>
2021-01-04 15:41:49 +0800

    [web 1.4.1.1]修正啟用bug和群組無法編輯圖片的bug

--- src/app/shared/version.ts
+++ src/app/shared/version.ts
@@ -1,6 +1,6 @@
-// fix time 2020/12/25
+// fix time 2020/01/04
 export const version = {
-  develop: '1.4.1.0 alpha',
-  release: '1.4.1.0 alpha',
+  develop: '1.4.1.1 alpha',
+  release: '1.4.1.1 alpha',
   master: '1.4.1'
 };
