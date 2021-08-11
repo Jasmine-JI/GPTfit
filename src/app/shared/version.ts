2b4f0cc51fb8bcf6f4a50949b26c065cb408331c
kidin_liu <dezhi0205@hotmail.com>
2021-08-11 10:04:40 +0800

    [web 1.7.2.1]註冊帳號流程調整

--- src/app/shared/version.ts
+++ src/app/shared/version.ts
@@ -1,6 +1,6 @@
 // fix time 2020/07/14
 export const version = {
-  develop: '1.7.1.0 alpha',
-  release: '1.7.1.0 alpha',
+  develop: '1.7.2.1 alpha',
+  release: '1.7.2.1 alpha',
   master: '1.7.2'
 };
