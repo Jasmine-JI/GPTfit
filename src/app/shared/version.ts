9df02e15ff938fe0f9e3033046258452b8993233
kidin_liu <dezhi0205@hotmail.com>
2022-07-20 08:54:29 +0800

    [web 1.12.0.2]站內信 bug 修復

--- src/app/shared/version.ts
+++ src/app/shared/version.ts
@@ -1,6 +1,6 @@
-// fix time 2022/07/15
+// fix time 2022/07/20
 export const version = {
-  develop: '1.12.0.1 alpha',
-  release: '1.12.0.1 alpha',
+  develop: '1.12.0.2 alpha',
+  release: '1.12.0.2 alpha',
   master: '1.12.0'
 };
