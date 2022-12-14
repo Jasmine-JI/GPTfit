97554b169dad2e9b371e4f3eb8b34a3f156cfa95
kidin_liu <dezhi0205@hotmail.com>
2022-12-14 13:57:39 +0800

    [web]update angular/core

--- src/test.ts
+++ src/test.ts
@@ -14,7 +14,6 @@ import {
 
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare const __karma__: any;
-declare const require: any;
 
 // Prevent Karma from running prematurely.
 __karma__.loaded = function () {};
@@ -23,9 +22,5 @@ __karma__.loaded = function () {};
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
   teardown: { destroyAfterEach: false },
 });
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
 // Finally, start Karma to run the tests.
 __karma__.start();
